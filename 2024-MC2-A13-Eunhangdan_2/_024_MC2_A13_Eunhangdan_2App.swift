1a4c3dd8e7304f633dac5d640d2accbd9202a8ab
Marty <chihyung0@naver.com>
2024-05-21 20:50:16 +0900

    Initial Commit

---
+++ 2024-MC2-A13-Eunhangdan_2/_024_MC2_A13_Eunhangdan_2App.swift
@@ -0,0 +1,17 @@
+//
+//  _024_MC2_A13_Eunhangdan_2App.swift
+//  2024-MC2-A13-Eunhangdan_2
+//
+//  Created by marty.academy on 5/21/24.
+//
+
+import SwiftUI
+
+@main
+struct _024_MC2_A13_Eunhangdan_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
