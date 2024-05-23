882a04a193eebb3bb6ec2374d2e5d41a9d0b015c
PARK SEONG HYEON <lovehyun95@gmail.com>
2024-05-24 00:33:28 +0900

    - 미니 피규어 메인, 테마, 서브 테마별 데이터 연동 - 변수명 수정

--- 2024-MC2-A13-Eunhangdan_2/View/Minifigure/MinifigureView.swift
+++ 2024-MC2-A13-Eunhangdan_2/View/Minifigure/MinifigureView.swift
@@ -7,54 +7,90 @@
 
 
 import SwiftUI
+import SwiftData
 //MARK: - 임시로 사용할 Struct 구현부
 struct minifigureItem: Identifiable {
     let id: UUID = .init()
     var minifigureImage: String
+    var minifigureSubTheme: String
+    var minifigureTheme: String
 }
 struct villageItem: Identifiable {
     let id: UUID = .init()
     var villageImageString: String
     var villageBackGroundColor : Color
 }
-//MARK: - 캐러셀 구현부
-struct Carousel<Content: View, Items: RandomAccessCollection>: View where Items.Element: Identifiable {
-    var data: Items
+//MARK: - Carousel 구현부
+struct Carousel<Content: View, minifigImages: RandomAccessCollection>: View where minifigImages.Element: Identifiable {
+    var minifigureImages: minifigImages
     var itemWidth: CGFloat
+    var itemHeight: CGFloat
     @Binding var activeID: UUID?
     @Binding var showMinifigureModal: Bool
-    @ViewBuilder var content: (Items.Element, Bool) -> Content
+    @ViewBuilder var content: (minifigImages.Element, Bool) -> Content
     var body: some View {
         GeometryReader {
             let size = $0.size
             VStack {
-                ScrollView(.horizontal) {
-                    LazyHStack(alignment: .center) {
-                        ForEach(data) { item in
-                            let isFocused = isItemFocused(item)
-                            Button(action: {
-                                showMinifigureModal = true
-                            }, label: {
-                                content(item, isFocused)
-                                    .frame(width: itemWidth)
-                                // .border(.black, width: 1)
-                                    .scrollTransition { content, phase in
-                                        content
-                                            .scaleEffect(
-                                                x: determineScaleEffect(phase.isIdentity),
-                                                y: determineScaleEffect(phase.isIdentity)
-                                            )
-                                    }
-                            })
+                if (itemWidth >= 100) {
+                    ScrollView(.horizontal) {
+                        LazyHStack(alignment: .center) {
+                            ForEach(minifigureImages) { image in
+                                let isFocused = isItemFocused(image)
+                                Button(action: {
+                                    showMinifigureModal = true
+                                }, label: {
+                                    content(image, isFocused)
+                                        .frame(height: itemHeight)
+                                        .scaledToFit()
+                                        .scrollTransition { content, phase in
+                                            content
+                                                .scaleEffect(
+                                                    x: determineScaleEffect(phase.isIdentity),
+                                                    y: determineScaleEffect(phase.isIdentity)
+                                                )
+                                        }
+                                })
+                            }
+                            .padding(10)
                         }
-                        .padding(10)
+                        .scrollTargetLayout()
+                        .padding(.horizontal,  (size.width - (itemWidth * 3.76)))
                     }
-                    .scrollTargetLayout()
-                    .padding(.horizontal,  (size.width - (itemWidth * 6.7)))
+                    .scrollIndicators(.hidden)
+                    .scrollTargetBehavior(.viewAligned)
+                    .scrollPosition(id: $activeID, anchor: .center)
+                }else{
+                    ScrollView(.horizontal) {
+                        LazyHStack(alignment: .center) {
+                            ForEach(minifigureImages) { image in
+                                let isFocused = isItemFocused(image)
+                                Button(action: {
+                                    showMinifigureModal = true
+                                }, label: {
+                                    content(image, isFocused)
+                                        .scaledToFit()
+                                        .frame(height: itemHeight)
+                                        .scrollTransition { content, phase in
+                                            content
+                                                .scaleEffect(
+                                                    x: determineScaleEffect(phase.isIdentity),
+                                                    y: determineScaleEffect(phase.isIdentity)
+                                                )
+                                        }
+                                })
+                            }
+                            .padding(10)
+                        }
+                        .scrollTargetLayout()
+                        .padding(.horizontal,  (size.width - (itemWidth * 6.7)))
+                    }
+                    .scrollIndicators(.hidden)
+                    .scrollTargetBehavior(.viewAligned)
+                    .scrollPosition(id: $activeID, anchor: .center)
+                    
                 }
-                .scrollIndicators(.hidden)
-                .scrollTargetBehavior(.viewAligned)
-                .scrollPosition(id: $activeID, anchor: .center)
+                
             }
         }
     }
@@ -63,11 +99,11 @@ struct Carousel<Content: View, Items: RandomAccessCollection>: View where Items.
         let scaleDefault = 1.0
         return isIdentity ? scalePoint : scaleDefault
     }
-    private func isItemFocused(_ item: Items.Element) -> Bool {
-        return item.id == activeID as? Items.Element.ID
+    private func isItemFocused(_ item: minifigImages.Element) -> Bool {
+        return item.id == activeID as? minifigImages.Element.ID
     }
 }
-//MARK: - 빌리지 캐러셀 구현부
+//MARK: - village Carousel 구현부
 struct villageCarousel<Content: View, Items: RandomAccessCollection>: View where Items.Element: Identifiable {
     var data: Items
     var itemWidth: CGFloat
@@ -83,7 +119,6 @@ struct villageCarousel<Content: View, Items: RandomAccessCollection>: View where
                     LazyHStack(alignment: .center) {
                         ForEach(data) { item in
                             let isFocused = isItemFocused(item)
-                            // 테마별로 클릭 시 해당 마을 꾸미기로 넘어감
                             Button(action: {
                                 
                             }, label: {
@@ -107,18 +142,20 @@ struct villageCarousel<Content: View, Items: RandomAccessCollection>: View where
         return item.id == activeID as? Items.Element.ID
     }
 }
-//MARK: - MinifigureView 구현부
+//MARK: - Minifigure View 구현부
 struct MinifigureView: View {
     var minifigureImage: String
     var isFocused: Bool = false
+    var legoHeight: CGFloat
     
     var body: some View {
         VStack {
             Image(minifigureImage)
                 .resizable()
                 .font(.body)
+                .scaledToFit()
                 .bold(isFocused)
-                .frame(width: 64, height: 104)
+                .frame(height: legoHeight)
         }
     }
 }
@@ -145,15 +182,15 @@ struct VillageView: View{
     }
 }
 
-//MARK: - 미니피규어 모달 정보창 구현 뷰
+//MARK: - Minifigure Modal View 구현부
 struct MinifigureModalView: View{
-    //to dismiss sheet
-    
+    let minifigID: String
+
     var body: some View {
         VStack{
             Spacer(minLength: 16.5)
             HStack(){
-                Text("Minifigure Detail")
+                Text("")
                     .font(.title2)
                     .bold()
                     .padding(.leading, 16)
@@ -165,7 +202,8 @@ struct MinifigureModalView: View{
                 Spacer(minLength: 24)
                 Image("avt011")
                     .resizable()
-                    .frame(width: 83, height: 131)
+                    .scaledToFit()
+                    .frame(height: 131)
                 Spacer(minLength: 28)
                 VStack{
                     HStack(){
@@ -183,16 +221,16 @@ struct MinifigureModalView: View{
                             .padding(.horizontal)
                     }
                     HStack(){
-                    Text("Pro.Name:")
-                        .multilineTextAlignment(.leading)
+                        Text("Pro.Name:")
+                            .multilineTextAlignment(.leading)
                         Spacer()
                         Text("Herry Poter")
                             .multilineTextAlignment(.leading)
                             .padding(.horizontal)
                     }
                     HStack(){
-                    Text("Release Date:")
-                        .multilineTextAlignment(.leading)
+                        Text("Release Date:")
+                            .multilineTextAlignment(.leading)
                         Spacer()
                         Text("Herry Poter")
                             .multilineTextAlignment(.leading)
@@ -214,6 +252,117 @@ struct MinifigureModalView: View{
                 .fill(Color(.gray).opacity(0.3))
                 .frame(width: 393, height: 156)
         }
+    }
+}
+// MARK: - Minifigure List View 구현부
+struct MinifigureListView: View {
+    @State var scrolledID: UUID?
+    @Binding var showMinifigureModal: Bool
+    @Binding var minifigures: [minifigureItem]
+    @State var subthemeFilteredMinifigs: [[minifigureItem]] = []
+    @State var subThemeArray: [String] = []
+    var body: some View {
+        ScrollView(){
+            ForEach(subThemeArray.indices, id: \.self){ index in
+                let subTheme = subThemeArray[index]
+                ZStack(){
+                    if index % 2 == 0{
+                        Rectangle()
+                            .fill(.white)
+                            .frame(width: 393, height: 180)
+                            .offset(x: 0, y: 0)
+                    }
+                    VStack(){
+                        HStack(){
+                            NavigationLink(
+                                destination: MinifigureShelfView(minifigures: $subthemeFilteredMinifigs[index], showMinifigureModal: $showMinifigureModal)){
+                                    Text("\(subTheme)")
+                                        .font(.title2)
+                                        .bold()
+                                        .tint(.black)
+                                        .padding(.leading, 20)
+                                    Image(systemName: "chevron.right")
+                                        .font(.system(size: 22))
+                                        .foregroundColor(.gray)
+                                    Spacer()
+                                }.padding(.bottom, 0)
+                        }
+                        ZStack(){
+                            Image("shelf")
+                                .resizable()
+                                .frame(width: 393, height: 60)
+                                .offset(x: 2, y: 110.0)
+                            HStack(){
+                                Carousel(minifigureImages: subthemeFilteredMinifigs[index], itemWidth: 57, itemHeight: 202, activeID: $scrolledID, showMinifigureModal: $showMinifigureModal) { item, isFocused in
+                                    MinifigureView(minifigureImage: item.minifigureImage, isFocused: isFocused, legoHeight: 170)
+                                }
+                            }.frame(width: 393, height: 180)
+                        }
+                        Spacer(minLength: 40)
+                    }
+                }
+            }
+            .padding(.top, 20)
+        }
+        .navigationTitle(minifigures.first?.minifigureTheme ?? "Default Title")
+        .navigationSplitViewStyle(.automatic)
+        .onAppear() {
+            var subThemeArray: [String] = []
+            minifigures.forEach {
+                subThemeArray.append($0.minifigureSubTheme)
+            }
+            let noDuplicateTheme = Set(subThemeArray)
+            self.subThemeArray = Array(noDuplicateTheme).sorted()
+            
+            for subTheme in self.subThemeArray {
+                let filteredMinifigs = minifigures.filter {
+                    $0.minifigureSubTheme == subTheme
+                }
+                subthemeFilteredMinifigs.append(filteredMinifigs)
+            }
+        }
+    }
+}
+//MARK: - Minifigure Shelf View 구현부
+struct MinifigureShelfView: View{
+    @Binding var minifigures: [minifigureItem]
+    @Binding var showMinifigureModal: Bool
+    var itemHeight: CGFloat = 104
+    let columns: [GridItem] = Array(repeating: .init(.flexible()), count: 4)
+    
+    var body: some View {
+        ScrollView {
+            VStack{
+                ZStack{
+                    LazyVGrid(columns: columns, spacing: 22) {
+                        ForEach(minifigures.indices, id: \.self) { index in
+                            ZStack(){
+                                if index % 4 == 0 {
+                                    Image("shelf")
+                                        .resizable()
+                                        .frame(width: 393, height: 50)
+                                        .offset(x: 151, y: 75)
+                                }
+                                HStack{
+                                    Button(action: {
+                                        showMinifigureModal = true
+                                    }, label: {
+                                        VStack{
+                                            Image(minifigures[index].minifigureImage)
+                                                .resizable()
+                                                .scaledToFit()
+                                                .frame(height: itemHeight)
+                                        }
+                                    })
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        .navigationTitle("String")
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
