c8a30108161539bba11b3347eba58d7cd80bb9b7
MartyTheGout <chihyung0@naver.com>
2024-05-23 12:24:09 +0900

    Revert "[MinifigureView -  AddMinifigure] Working on Views, API for Registering a Minifigure."
    This reverts commit 6942be3f2f7f2780ffbc43a7ea270b5f576f5678.

--- 2024-MC2-A13-Eunhangdan_2/Helper/Constants.swift
+++ 2024-MC2-A13-Eunhangdan_2/Helper/Constants.swift
@@ -10,7 +10,6 @@ import Foundation
 public struct BrickApi {
     static let requestUrl = "https://brickset.com/api/v3.asmx"
     static let getSets = "/getSets?"
-    static let setMinifigCollection = "/setMinifigCollection?"
     static let getMinifigCollection = "/getMinifigCollection?"
     static let apiKey = "3-GcQZ-hWm5-zxb4C"
     static let userHash = "3L3kC2zWpk"
