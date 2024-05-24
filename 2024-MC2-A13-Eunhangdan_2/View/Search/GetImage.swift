d76f9424080a4b7004861480c0fd3a40214d715a
Minkyeong-Choi <minkyeong.choi@outlook.com>
2024-05-25 01:55:02 +0900

    [View - Search] 지난 pr 관련 수정사항입니다.

---
+++ 2024-MC2-A13-Eunhangdan_2/View/Search/GetImage.swift
@@ -0,0 +1,24 @@
+import Foundation
+import SwiftUI
+
+@ViewBuilder
+func getImage(id: String, url: String) -> some View {
+    if let imageExists = UIImage(named: "\(id)") {
+        Image("\(id)")
+            .resizable()
+            .scaledToFit()
+            .frame(width: 150, height: 110)
+            .clipped()
+    } else {
+        //TODO: getting Data and show as Image later
+        AsyncImage(url: URL(string: "\(url)")) {image in
+            image
+                .resizable()
+                .scaledToFit()
+        } placeholder: {
+            // Progress View
+        }
+        .frame(width: 150, height: 110)
+        .clipped()
+    }
+}
---
+++ 2024-MC2-A13-Eunhangdan_2/View/Search/MinifigureDetailView.swift
@@ -0,0 +1,81 @@
+import Foundation
+import SwiftUI
+import SwiftData
+
+struct MinifigureDetailView: View {
+    @Environment (\.modelContext) private var modelContext
+//    @State var minifigureID: String = ""
+//    @Binding var id: String
+    let mini: Minifig
+    @State private var filteredMinifigureList: [Minifig] = []
+    @Query(sort: \Minifig.minifigName) var minifigInfo: [Minifig]
+
+    var body: some View {
+            VStack {
+                if let imageExists = UIImage(named: "\(mini.minifigID)") {
+                    Image("\(mini.minifigID)")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 300, height: 300)
+                        .clipped()
+                } else {
+                    //TODO: getting Data and show as Image later
+                    AsyncImage(url: URL(string: "\(mini.minifigImageURL)")) {image in
+                        image
+                            .resizable()
+                            .scaledToFit()
+                    } placeholder: {
+                        // Progress View
+                    }
+                    .frame(width: 300, height: 300)
+                    .clipped()
+                }
+                HStack {
+                    Text("\(mini.themeCategory)")
+                        .font(.title.weight(.semibold))
+                    Spacer()
+                }.multilineTextAlignment(.leading)
+                Text("").frame(height: 30)
+                HStack {
+                    Text("Name: ")
+                        .foregroundColor(.gray)
+                    Text("\(mini.minifigName)")
+                        .font(.subheadline)
+                    Spacer()
+                }.multilineTextAlignment(.leading)
+                Text("")
+                HStack {
+                    Text("Released year: ")
+                        .foregroundColor(.gray)
+                    Text("\(mini.minifigID)")
+                        .font(.subheadline)
+                    Spacer()
+                }.multilineTextAlignment(.leading)
+
+//                Divider()
+//                HStack {
+//                    Text("Series")
+//                        .font(.title.weight(.semibold))
+//                    Spacer()
+//                }
+            }.onAppear{loadMinifigureData(id: mini.minifigID)}
+            .padding()
+    }
+    
+    private func loadMinifigureData(id: String) {
+        let minifigurePredicate = #Predicate<Minifig> {
+            $0.minifigID == id
+        }
+        
+        let minifigureDescriptor = FetchDescriptor<Minifig>(predicate: minifigurePredicate)
+        
+        do {
+            filteredMinifigureList = try modelContext.fetch(minifigureDescriptor)
+        } catch {
+            print("Failed to fetch data: \(error)")
+        }
+        
+    }
+
+}
+
