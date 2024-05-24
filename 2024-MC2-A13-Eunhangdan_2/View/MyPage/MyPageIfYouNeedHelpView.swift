cce80e926c8b91b8d96801c2cc98f699eff1c2f3
Gimli Kim <infinite1st@gmail.com>
2024-05-24 20:52:10 +0900

    [MyPageView - View] MyPageView 최종 및 아이콘 추가

--- 2024-MC2-A13-Eunhangdan_2/View/MyPage/MyPageIfYouNeedHelpView.swift
+++ 2024-MC2-A13-Eunhangdan_2/View/MyPage/MyPageIfYouNeedHelpView.swift
@@ -9,8 +9,17 @@ import SwiftUI
 
 struct MyPageIfYouNeedHelpView: View {
     var body: some View {
-        
+        Image(systemName: "phone.bubble")
+            .resizable()
+            .frame(width: 50, height: 50)
+            .foregroundColor(.green)
         Text("김경림 전화번호: 010 3299 2435")
+            .font(.title3)
+            .foregroundColor(.brown)
+            .bold()
+        Text("여기로 전화하세요!")
+            .font(.headline)
+            .foregroundColor(.brown)
     }
 }
 
